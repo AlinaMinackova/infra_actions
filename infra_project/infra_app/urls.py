47cdfd2e6b936d0b5cf7894e0077e27a4835068d
AlinaMinakova <94383135+AlinaMinackova@users.noreply.github.com>
2025-06-22 10:18:08 +0300

    Add flake8 check

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -9,3 +9,8 @@ urlpatterns = [
     path('second/', views.second_page, name='second_page'),
 
 ]
+# для проверки на pep8
+# pip install flake8 pep8-naming flake8-broken-line flake8-return flake8-isort
+# flake8 .
+# исправить недочеты в папке (если не можем найти сами (можно и все сразу))
+# isort infra_project/urls.py
